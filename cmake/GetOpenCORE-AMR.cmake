4f7ec7142de7eb863f7395121ec7a488ba2b295b
Andrei Fedorov <anfedoro@gmail.com>
2024-12-26 10:05:30 +0000

    amr_wb added

--- cmake/GetOpenCORE-AMR.cmake
+++ cmake/GetOpenCORE-AMR.cmake
@@ -16,15 +16,33 @@ ExternalProject_Add(opencore-amr
     TEST_EXCLUDE_FROM_MAIN ON
     INSTALL_COMMAND make install
     INSTALL_DIR ${INSTALL_DIR}
-    BUILD_BYPRODUCTS ${INSTALL_DIR}/lib/libopencore-amrnb.${LIBRARY_SUFFIX}
+    BUILD_BYPRODUCTS
+        ${INSTALL_DIR}/lib/libopencore-amrnb.${LIBRARY_SUFFIX}
+        ${INSTALL_DIR}/lib/libopencore-amrwb.${LIBRARY_SUFFIX}
 )
 
 file(MAKE_DIRECTORY ${INCLUDE_DIR})
 
-add_library(AMR::lib STATIC IMPORTED GLOBAL)
-set_property(TARGET AMR::lib PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libopencore-amrnb.${LIBRARY_SUFFIX})
-#set_property(TARGET AMR::lib PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libopencore-amrwb.${LIBRARY_SUFFIX})
-set_property(TARGET AMR::lib PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INCLUDE_DIR})
-set_property(TARGET AMR::lib PROPERTY INTERFACE_COMPILE_DEFINITIONS HAVE_OPENCORE_AMR)
-add_dependencies(AMR::lib opencore-amr)
+# Create imported target for NB
+add_library(AMR::libNB STATIC IMPORTED GLOBAL)
+set_property(TARGET AMR::libNB PROPERTY IMPORTED_LOCATION
+    ${INSTALL_DIR}/lib/libopencore-amrnb.${LIBRARY_SUFFIX})
+set_property(TARGET AMR::libNB PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INCLUDE_DIR})
+set_property(TARGET AMR::libNB PROPERTY INTERFACE_COMPILE_DEFINITIONS HAVE_OPENCORE_AMR)
+add_dependencies(AMR::libNB opencore-amr)
 
+# Create imported target for WB
+add_library(AMR::libWB STATIC IMPORTED GLOBAL)
+set_property(TARGET AMR::libWB PROPERTY IMPORTED_LOCATION
+    ${INSTALL_DIR}/lib/libopencore-amrwb.${LIBRARY_SUFFIX})
+set_property(TARGET AMR::libWB PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${INCLUDE_DIR})
+set_property(TARGET AMR::libWB PROPERTY INTERFACE_COMPILE_DEFINITIONS HAVE_OPENCORE_AMR)
+add_dependencies(AMR::libWB opencore-amr)
+
+# Optionally create a single 'AMR' INTERFACE target that links to both NB and WB
+add_library(AMR_lib INTERFACE)
+target_link_libraries(AMR_lib INTERFACE AMR::libNB AMR::libWB)
+target_include_directories(AMR_lib INTERFACE ${INCLUDE_DIR})
+target_compile_definitions(AMR_lib INTERFACE HAVE_OPENCORE_AMR)
+add_dependencies(AMR_lib opencore-amr)
+add_library(AMR::lib ALIAS AMR_lib)
